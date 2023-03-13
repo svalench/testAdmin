ad1d2cb169b12d10bdced978305639432c784c8b
svalench <chitsalex@gmail.com>
2023-03-13 23:05:40 +0300

    админка

--- components/admin.py
+++ components/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from components.models import StatusUser, StatusDocType, StatusWorkType, StatusCalculation, Components
+
+
+class ComponentsAdmin(admin.ModelAdmin):
+    model = Components
+    list_display = ["id", "name", "data_id"]
+
+
+class StatusUserAdmin(admin.ModelAdmin):
+    model = StatusUser
+    list_display = ["id", "name", ]
+
+
+class StatusDocTypeAdmin(admin.ModelAdmin):
+    model = StatusDocType
+    list_display = ["id", "name", ]
+
+
+class StatusWorkTypeAdmin(admin.ModelAdmin):
+    model = StatusWorkType
+    list_display = ["id", "name", ]
+
+
+class StatusCalculationAdmin(admin.ModelAdmin):
+    model = StatusCalculation
+    list_display = ["id", "name", ]
+
+
+admin.site.register(Components, ComponentsAdmin)
+admin.site.register(StatusUser, StatusUserAdmin)
+admin.site.register(StatusDocType, StatusDocTypeAdmin)
+admin.site.register(StatusWorkType, StatusWorkTypeAdmin)
+admin.site.register(StatusCalculation, StatusCalculationAdmin)
