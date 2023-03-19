c6e475f58f1ae3a05b1c3a1d52c2d1a2e18dbb5f
svalench <chitsalex@gmail.com>
2023-03-19 19:25:40 +0300

    many_to_many get from  model

--- components/views.py
+++ components/views.py
@@ -1,66 +1,10 @@
 from django.shortcuts import render
 from django.apps import apps
 
-from components.models import Components
+from components.models import Components, BaseChoiceClassStatuses
 
-DOCUMENTS_TYPES = [
-    {"name": "Тип документа ГИ", "id": "db"},
-    {"name": "Тип документа EXCEL", "id": "excel"},
-    {"name": "Для всех", "id": "all"},
-]
-
-TYPES_WORK = [
-    {"name": "Мой в работе", "id": "work_my"},
-    {"name": "Чужой в работе", "id": "work_stranger"},
-    {"name": "Ничей", "id": "not_work"},
-]
-
-USERS_ROLES = [
-    {"name": "Все СБЕ", "id": "all_sbe"},
-    {"name": "СБЕ", "id": "sbe"},
-]
-
-DOC_STATUS = [
-    {"name": "Новый", "id": "new"},
-    {"name": "Загружен в САП", "id": "in_SAP"},
-    {"name": "Главный расчет", "id": "main_calculation"},
-]
-
-COMPONENTS = [
-    {"id": 1, "data_id": "Ststus1", "name": "Кноипка 1"},
-    {"id": 2, "data_id": "Ststus2", "name": "Кноипка 2"},
-    {"id": 3, "data_id": "Ststus3", "name": "Кноипка 3"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-    {"id": 4, "data_id": "InputType", "name": "поле ввода"},
-]
 
 def components_view(request):
-    STAT = {
-        "DOCUMENTS_TYPES": DOCUMENTS_TYPES,
-        "TYPES_WORK": TYPES_WORK,
-        "DOC_STATUS": DOC_STATUS,
-        "USERS_ROLES": USERS_ROLES
-    }
     model = request.GET.get('model', 'StatusUser')
     Model = apps.get_model(app_label='components', model_name=model)
     ctx = {
@@ -68,6 +12,6 @@ def components_view(request):
         'many_to_many': next(iter(Model._meta.fields_map.keys())),
         "COMPONENTS": Components.objects.all(),
         "STATUSES": Model.objects.all(),
-        "ALL_STATUSES": [*DOCUMENTS_TYPES, *TYPES_WORK, *DOC_STATUS]
+        'statuses': BaseChoiceClassStatuses.StatusesComponents,
     }
     return render(request, 'admin_custom_page.html', ctx)
